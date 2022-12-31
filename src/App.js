9670035807999191c5b7fff74580860571e0b607
shobhit-u <shobhitumrao5@gmail.com>
2022-12-31 15:30:57 +0530

    cart-ui redux complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 import { Cart, Header, Item } from "./container";
+import { useSelector } from "react-redux";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartVisible);
   return (
     <>
       <Header />
-      <Cart />
+      {showCart && <Cart />}
       <Item />
     </>
   );
--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Header.css";
 
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/uiSlice";
+
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
     <div className="header">
       <p className="logo">Redux Shopping Cart</p>
-      <button className="btn">
+      <button onClick={toggleCartHandler} className="btn">
         My Cart <span className="cart-quantity">1</span>
       </button>
     </div>
