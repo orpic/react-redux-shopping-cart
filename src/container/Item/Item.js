fd26ea9877e536c0a1b07522b4f92c21c7decee6
shobhit-u <shobhitumrao5@gmail.com>
2022-12-31 15:00:10 +0530

    product-item complete

--- src/container/Item/Item.js
+++ src/container/Item/Item.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./Item.css";
 
 const Item = () => {
-  return <div>Item</div>;
+  return (
+    <>
+      <div className="container-center">
+        <div className="prod-item-box">
+          <div className="prod-item-row">
+            <div className="prod-item-col pr-it-col-left">
+              <p className="prod-item-name">Test Product 1</p>
+              <p className="prod-item-desc">
+                this is a first product - amazing
+              </p>
+            </div>
+            <div className="prod-item-col pr-it-col-right">
+              <p className="prod-item-price">₹10.00</p>
+              <p className="prod-item-add-to-cart btn">add to cart</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Item;
